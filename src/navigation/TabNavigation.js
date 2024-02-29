cb5839c022bf84a20534a675b7f130693b22eedb
Ramiro Landajo <ramalandajo@gmail.com>
2024-02-29 11:04:35 -0300

    Initial state app

---
+++ src/navigation/TabNavigation.js
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+
+export default function TabNavigation() {
+    return (
+        <View>
+            <Text>TabNavigation</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({})
